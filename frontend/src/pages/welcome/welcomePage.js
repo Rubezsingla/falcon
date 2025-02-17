0c8fac895b0e373977c87a35ed86588e2d5be06c
rupika9124 <rupika91204@gmail.com>
2025-02-18 04:52:17 +0530

    css

--- frontend/src/pages/welcome/welcomePage.js
+++ frontend/src/pages/welcome/welcomePage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 import './Welcome.css';
 const openExerciseApp = () => {
     window.open("http://localhost:3001", "_blank"); // Replace with actual deployed URL
   };
 
 function WelcomePage() {
+    const { user, logout } = useContext(AuthContext);
     const navigate = useNavigate();
 
     const handleNavigation = (path) => {
@@ -19,7 +22,14 @@ function WelcomePage() {
                 <button onClick={() => handleNavigation('/')}>Home</button>
                 <button onClick={() => handleNavigation('/diet-chart')}>Diet Chart</button>
                 <button onClick={() => handleNavigation('/challenge-friend')}>Challenge a Friend</button>
-                <button onClick={() => handleNavigation('/login')}>Logout</button>
+                {user ? (
+          <div className="navItems">
+            <span className="username">Welcome,  {user.username}</span>
+          </div>
+        ) : (
+          <div className="navItems"> <p>Welcome, Fitness Warrior!</p>
+          </div>
+        )}
             </nav>
             {/* Background Video */}
             <video autoPlay loop muted className="background-video">
